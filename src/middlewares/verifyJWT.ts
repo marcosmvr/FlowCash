70e3f23930cfd7988c8825cdcc344e71f3d46ea7
marcosmvr <marcosviniciusreis260@gmail.com>
2025-04-12 20:51:38 -0300

    middleware para proteger rotas com token criado

---
+++ src/middlewares/verifyJWT.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export const verifyJWT = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const authHeader = request.headers['authorization']
+  if (!authHeader) {
+    return reply.status(401).send({ message: 'Token não fornecido' })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, 'seu-segredo-aqui') as {
+      userId: number
+      email: string
+    }
+    request.user = decoded
+
+    return true
+  } catch (error) {
+    return reply.status(401).send({ message: 'Token inválido' })
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from 'fastify'
 import { cashRegisterRoute } from './routes/cashRegisterRoutes'
+import { authRoutes } from './routes/cashRegisterAuthRoutes'
 import dotenv from 'dotenv'
 
 dotenv.config()
 const app = fastify()
 
+app.register(authRoutes, { prefix: '/cash-register' })
 app.register(cashRegisterRoute, { prefix: '/cash-register' })
 
 app.listen({ port: 3333 }, function (err, address) {
